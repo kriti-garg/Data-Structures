b0903c446bccebb10d60fd7424c95b4dc3f8adc9
kriti-garg <kritigarg1998@gmail.com>
2018-07-24 20:53:12 +0530

    add doublyLinkedList

--- doublyLinkedList.cpp
+++ doublyLinkedList.cpp
@@ -1,17 +1,55 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 struct node
 {
     int data;
     node *next;
+    node *prev;
 };
 struct node *head = NULL;
-    void insertEnd()
-    {
-        node *temp = new node;
+void insertFront(){
+	    node *temp = new node;
+       
+        cout<<"enter the value you want to insert";
+        cin>>temp->data;
+        temp->prev=NULL;
+        temp->next = NULL;
+        if(head==NULL){
+            head = temp;
+        }
+        else{
+            temp->next = head;
+            head->prev = temp;
+            head = temp;
+        }
+}
+void insertMiddle(){
+	 node *temp = new node;
+        int val;
         cout<<"enter the value you want to insert";
         cin>>temp->data;
         temp->next = NULL;
+        cout<<"enter the value after which you want to insert";
+        cin>>val;
+        node *t = head;
+        if(head==NULL){
+        	cout<<"List is empty\n";
+        }
+        else{
+        while(t->data!=val)
+            t = t->next;
+            temp->next=t->next;
+            t->next=temp;
+            temp->prev = t;
+            t->next->prev=temp->prev;
+        }
+    }
+void insertEnd(){
+	 node *temp = new node;
+        cout<<"enter the value you want to insert";
+        cin>>temp->data;
+        temp->next = NULL;
+        temp->prev=NULL;
         if(head == NULL)
         {
             head = temp;
@@ -20,94 +58,75 @@ struct node *head = NULL;
         {
             node *t = head;
             while(t->next!= NULL)
-            t =t->next;
-            t->next = temp;
+                 t =t->next;
+             t->next=temp;
+             temp->prev=t;
         }
-    }
-    void insertMiddle(){
-        node *temp = new node;
-        int val;
-        cout<<"enter the value you want to insert";
-        cin>>temp->data;
-        temp->next = NULL;
-        cout<<"enter the value after which you want to insert";
-        cin>>val;
-        node *t = head;
-        bool flag=0;
-        while(t->data!=val){
-            t = t->next;
-        }
-        if(t->data==val)
-            flag=1;
-        if(flag==1){
-        temp->next = t->next;
-        t->next = temp;
-    }
-    }
-    void insertFront(){
-        node *temp = new node;
-        int val;
-        cout<<"enter the value you want to insert";
-        cin>>temp->data;
-        node *t = head;
-        temp->next = NULL;
-        if(head==NULL){
-            head = temp;
-        }
-        else{
-            node *t = head;
-            temp->next = t;
-            head = temp;
-        }
-    }
-    void deleteFront(){
-        node *t = new node();
+
+}
+void deleteFront(){
+	 node *t = new node();
         if(head==NULL){
             cout<<endl<<"List is empty";
         }
         else{
             t = head;
             head = t->next;
+            t->next->prev=NULL;
             delete t; 
         }
 
-    }
-    void deleteMiddle(){
-        
-        int val;
-        cout<<"\nenter the value you want to delete?\n";
-        cin>>val;
-        if(head == NULL){
-            cout<<endl<<"List is empty";
-        }
-        else{
-            node *t = head;
-            node *temp;
-            while(t->data!=val){
-                 temp=t;
-                t= t->next;
-                }
-            temp->next=t->next;
-            temp = temp->next;
-            delete t;
-        }
-    }
-    void deleteEnd(){
-        
-        if(head == NULL){
-            cout<<"List is Empty\n";
-        }
-        else{
-            node *t= head;
-           
-            while(t->next && t->next->next!=NULL){
-                t= t->next;
-            }
-            delete t->next;
-            t->next=NULL;
-        }
-         }
-    void display(){
+}
+void deleteMiddle(){
+	int pos,count=0;
+	node *ptr,*temp,*n=head;
+	cout<<"enter the position";
+	cin>>pos;
+	while(n!=NULL){
+		count++;
+		n= n->next;
+	}
+	if(pos<=count){
+	if(pos==0)
+		deleteFront();
+	else{
+		ptr=head;
+		for(int i=0;i<pos;i++){
+			temp = ptr;
+			ptr= ptr->next;
+			if(ptr==NULL){
+				cout<<"NULL value";
+			}
+			
+		}
+		temp->next = ptr->next;
+		ptr->next->prev=temp;
+		delete ptr;
+	}
+}
+else
+{
+	cout<<"wrong position";
+}
+
+
+}
+void deleteEnd(){
+	node *t = new node();
+	if(head==NULL){
+		cout<<endl<<"List is empty";
+	}
+	else{
+		node *t = head;
+		while(t->next && t->next->next!=NULL)
+			t= t->next;
+		delete t->next;
+		t->next=NULL;
+
+	}
+
+}
+void display(){
          node* n=head;
          if(n==NULL){
             cout<<"List is Empty";
@@ -168,6 +187,5 @@ int main()
         }
     }
 }
-    
-    return 0;
+return 0;
 }
