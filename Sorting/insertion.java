e7eee2560b763781bb21cb2f50371cf2b24f7676
kriti-garg <kritigarg1998@gmail.com>
2018-10-02 00:15:14 +0530

    modify insertion.java

--- Sorting/insertion.java
+++ Sorting/insertion.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 class insertion{
-	public static void main(String []args){
+		public static void main(String []args){
 		Scanner sc = new Scanner(System.in);
 		int num,size,key,i;
 		num = sc.nextInt();
@@ -8,17 +8,18 @@ class insertion{
 		for(i=0;i<num;i++){
 			arr[i] = sc.nextInt();
 		}
-		for(int j=2;j<arr.length;j++) {
+		for(int j=1;j<arr.length;j++) {
 			key = arr[j];
 			i=j-1;
-			while(i>0 && arr[i]>key) {
+			while(i>=0 && arr[i]>key) {
 				arr[i+1]=arr[i];
 				i=i-1;
 				arr[i+1] = key;
 			}
 		}
 		for(int in=0;in<num;in++) {
-			System.out.print(arr[i] + " ");
+			System.out.print(arr[in] + " ");
 		}
+		sc.close();
 	}
 }
