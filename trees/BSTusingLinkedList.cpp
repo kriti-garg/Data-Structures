9d72aeabadf5b436d1e4a1075b0c52ae6868635d
kriti-garg <kritigarg1998@gmail.com>
2018-10-07 15:34:57 +0530

    Modify BST insertion

--- trees/BSTusingLinkedList.cpp
+++ trees/BSTusingLinkedList.cpp
@@ -1,48 +1,54 @@
 #include<iostream>
 using namespace std;
 struct tree{
-    int data;
-   struct tree* left;
-   struct tree* right;
+  int data;
+  struct tree* left=NULL;
+  struct tree* right=NULL;
 };
+
 tree* find(tree* root,int val){
-    tree* temp = root;
-    if(temp==NULL)
+  tree * temp = root;
+  if(temp==NULL)
     return temp;
-    else if(temp->data == val)
+  
+  else if(temp->data == val)
     return temp;
-    else if(temp->data>val){
-        if(temp->left==NULL)
-        return temp;
-        return find(temp->left,val);
-    }
-    else if(temp->data<val){
-        if(temp->right==NULL)
-        return temp;
-        return find(temp->right,val);
-    }
+  
+  else if(temp->data > val){
+    if(temp->left==NULL)
+      return temp;
+    return find(temp->left,val);
+  }
+  
+  if(temp->right==NULL)
+      return temp;
+  return find(temp->right,val);
 }
-void insert(tree** root,int val){
-    tree* place = find(*root,val);
-    if(place==NULL){
-        tree* start = new tree;
-        start->data = val;
-        start->left = NULL;
-        start->right = NULL;
-    }
-    else if(place->data>=val){
-        tree *temp=place->left;
-       // temp = new tree;
-        temp->data = val;
-        temp->left = NULL;
-        temp->right = NULL;
-    }
-    else if(place->data<val){
-        tree *temp=place->right;
-        temp->data = val;
-        temp->left = NULL;
-        temp->right = NULL;
-    }
+
+void insert(tree ** root,int val){
+  tree * place = find(*root,val);
+  if(place==NULL){
+    tree * start = new tree;
+    start->data = val;
+    start->left = NULL;
+    start->right = NULL;
+    *root = start;
+  }
+  else if(place->data>=val){
+    // tree *temp=place->left;
+    place->left = new tree;
+    tree *temp=place->left;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+  }
+  else if(place->data<val){
+    place->right = new tree;
+    tree *temp=place->right;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+  }
 }
 void inorder(tree *root){
     if(root!=NULL){
@@ -66,25 +72,23 @@ void postorder(tree *root){
     }
 }
 int main(){
-    int size,val,choice;
-    cin>>size;
-    tree *root = NULL;
-    for(int i=0;i<size;i++)
-    {   cin>>val;
-        insert(&root,val);
+  int val,choice;
+  tree *root = NULL;
+  cout<<"enter the choice :"<<endl;
+  cout<<"1: insertion"<<endl;
+  cout<<"2: inorder"<<endl<<"3: preorder"<<endl<<"4: postorder"<<endl;
+  
+  while(1){
+    cin >> choice;
+    if(choice==1){
+      cin >> val;
+      insert(&root,val);
     }
-    //inorder(root);
-    cout<<endl<<"enter the choice : "<<endl;
-    cout<<"1: inorder"<<endl<<"2: preorder"<<endl<<"3: postorder"<<endl;
-    while(1){
-    cin>>choice;
-    if(choice==1)
-        inorder(root);
     else if(choice==2)
-        preorder(root);
+      inorder(root);
     else if(choice==3)
-        postorder(root);
-    }
+      preorder(root);
+    else if(choice==4)
+      postorder(root); 
+  }
 }
-
-
